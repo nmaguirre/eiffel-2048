a5ef709308547ec66d9d03c1fdd626d2e3d001b6
lputruele <lputruele@gmail.com>
2013-09-08 17:53:56 -0300

    pruebabranch

---
+++ practico_git_IS/pruebabranch.java
@@ -0,0 +1,12 @@
+public class sumatoria {
+
+	public int void main(String[] args){
+		Lista l = new ListaSobreArreglos();
+		int index = 0;
+		while (index<10) {
+			l.insertar(index,index);
+			index++;
+		}
+		//hacer sumatoria
+	}
+}
